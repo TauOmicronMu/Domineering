c7b4fb529b84cdc82fa2fe4a4ae3a85f3658ff6c
Kelsey McKenna <ToastNumber@users.noreply.github.com>
2016-03-15 11:38:02 +0000

    Update DomineeringMove.java

--- src/DomineeringMove.java
+++ src/DomineeringMove.java
@@ -10,4 +10,17 @@ public class DomineeringMove {
     public String toString() {
     	return "(" + posA + "," + posB + ")";
     }
+    
+    @Override
+    public int hashCode() {
+        return (posA.hashCode() << 10) + posB.hashCode();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof DomineeringMove) {
+            DomineeringMove otherMove = (DomineeringMove) obj;
+            return otherMove.posA.equals(this.posA) && otherMove.posB.equals(this.posB);
+        } else return false;     
+    }
 }
