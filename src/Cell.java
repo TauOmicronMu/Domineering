ded21aaf2d59581df1de297a2f5f55325efd9c90
Kelsey McKenna <ToastNumber@users.noreply.github.com>
2016-03-15 11:36:29 +0000

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -11,4 +11,17 @@ public class Cell {
 	public String toString() {
 		return "(" + column + "," + row + ")";
 	}
+	
+	@Override
+	public int hashCode() {
+		return (column << 20) + row;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Cell) {
+			Cell otherCell = (Cell) obj;
+			return otherCell.column = this.column && otherCell.row = this.row;
+		} else return false;
+	}
 }
