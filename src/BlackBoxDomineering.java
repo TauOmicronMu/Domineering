bb5bef5a59ab5a140bd5f365b5496f3711dd4078
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2016-03-29 11:03:22 +0100

    Further work on BBD

--- src/BlackBoxDomineering.java
+++ src/BlackBoxDomineering.java
@@ -3,6 +3,61 @@ import java.util.Scanner;
 /**
  * Created by tom on 25/03/16.
  */
+
+/**
+ - It takes EXACTLY four command line arguments (and you should
+ have an assertion for the number of command line arguments).
+
+ - The first argument is either "first" or "second", indicating
+ whether your black box plays first or second.
+
+ - The second argument is either "vertical" or "horizontal",
+ indicating whether your black box is the player H or the player V.
+
+ (After discussion with the students on 11-12 of March, we are
+ keeping this specification, but we promise to use your black box
+ with the combinations first-H and second-V only, and hence you
+ don't need to implement the combinations first-V and second-H.)
+
+ - The third argument is the width of the board.
+
+ - The fourth argument is the height of the board.
+
+ - The main method then starts to play as the appropriate player as follows.
+
+ - It should print what we tell you below, and nothing else, because we
+ are going to parse the output and give you marks based on that.
+
+ - Anything you want to print for your own benefit, such as debugging,
+ should be printed to "System.err" rather than "System.out".
+
+ - Your program will read opponent moves from System.in, and write its
+ moves to System.out. It is important here that the reading and
+ writing is WITHOUT BUFFERING. After writing each move, you should
+ flush() the output.
+
+ - Each move in the input or the output is terminated by a new line.
+
+ - If your opponent plays an invalid move, you should exit with
+ System.exit(1). You may print a message in System.err, but
+ definitely not System.out.
+
+ - A move is a pair of numbers separated by comma, such as "3,5", in
+ ZERO-BASED coordinates.
+
+ - A vertical move x,y covers the positions x,y and x,(y+1).
+
+ - A horizontal move x,y covers the positions x,y and (x+1),y.
+
+ - We emphasize again your program should NOT PRINT ANYTHING ELSE to
+ standard output. Prompts or boards or debugging output or error
+ messages MUST go to System.err.
+
+ - When the game has finished because there are no available moves for
+ the next player, your program should end silently without error or
+ output other than System.err.
+
+ */
 public class BlackBoxDomineering {
 
     static String firstOrSecond;
@@ -10,61 +65,6 @@ public class BlackBoxDomineering {
     static int m;
     static int n;
     static Scanner scanner = new Scanner(System.in);
-
-    /**
-     - It takes EXACTLY four command line arguments (and you should
-     have an assertion for the number of command line arguments).
-
-     - The first argument is either "first" or "second", indicating
-     whether your black box plays first or second.
-
-     - The second argument is either "vertical" or "horizontal",
-     indicating whether your black box is the player H or the player V.
-
-     (After discussion with the students on 11-12 of March, we are
-     keeping this specification, but we promise to use your black box
-     with the combinations first-H and second-V only, and hence you
-     don't need to implement the combinations first-V and second-H.)
-
-     - The third argument is the width of the board.
-
-     - The fourth argument is the height of the board.
-
-     - The main method then starts to play as the appropriate player as follows.
-
-     - It should print what we tell you below, and nothing else, because we
-     are going to parse the output and give you marks based on that.
-
-     - Anything you want to print for your own benefit, such as debugging,
-     should be printed to "System.err" rather than "System.out".
-
-     - Your program will read opponent moves from System.in, and write its
-     moves to System.out. It is important here that the reading and
-     writing is WITHOUT BUFFERING. After writing each move, you should
-     flush() the output.
-
-     - Each move in the input or the output is terminated by a new line.
-
-     - If your opponent plays an invalid move, you should exit with
-     System.exit(1). You may print a message in System.err, but
-     definitely not System.out.
-
-     - A move is a pair of numbers separated by comma, such as "3,5", in
-     ZERO-BASED coordinates.
-
-     - A vertical move x,y covers the positions x,y and x,(y+1).
-
-     - A horizontal move x,y covers the positions x,y and (x+1),y.
-
-     - We emphasize again your program should NOT PRINT ANYTHING ELSE to
-     standard output. Prompts or boards or debugging output or error
-     messages MUST go to System.err.
-
-     - When the game has finished because there are no available moves for
-     the next player, your program should end silently without error or
-     output other than System.err.
-
-     */
 
     private static class CommandLineDom implements MoveChannel<DomineeringMove> {
 
@@ -77,9 +77,11 @@ public class BlackBoxDomineering {
 
             switch(vertOrHoriz) {
                 case "vertical" :
-                    return new DomineeringMove(x, y, Player.MAXIMIZER);
+                    DomineeringMove moveV = new DomineeringMove(x, y, Player.MINIMIZER);
+                    return moveV;
                 case "horizontal" :
-                    return new DomineeringMove(x, y, Player.MAXIMIZER);
+                    DomineeringMove moveH = new DomineeringMove(x, y, Player.MAXIMIZER);
+                    return moveH;
                 default :
                     System.err.println("RETURNED A NULL MOVE - WAS GIVEN NEITHER VERTICAL NOR HORIZONTAL.");
                     return null;
@@ -87,7 +89,8 @@ public class BlackBoxDomineering {
         }
 
         public void giveMove(DomineeringMove move) {
-            System.err.println("I play " + move);
+            System.out.println(move.x + "," + move.y);
+            System.out.flush();
         }
 
         public void comment(String msg) {
@@ -110,21 +113,18 @@ public class BlackBoxDomineering {
 
         DomineeringBoard board = new DomineeringBoard(m,n);
 
-        board.tree().firstPlayer(new CommandLineDom());
-        board.tree().secondPlayer(new CommandLineDom());
-
-        /*
         switch(firstOrSecond) {
             case "first" :
+                System.err.println("1st");
                 board.tree().firstPlayer(new CommandLineDom());
                 break;
             case "second" :
+                System.err.println("2nd");
                 board.tree().secondPlayer(new CommandLineDom());
                 break;
             default :
                 System.err.println("1st arg was wrong!");
                 System.exit(1);
         }
-        */
     }
 }
--- src/GameTree.java
+++ src/GameTree.java
@@ -91,7 +91,7 @@ public class GameTree<Move> {
     else {
       Move m = c.getMove();
       assert(children.containsKey(m));
-      System.out.println(children.containsKey(m));
+      //System.out.println(children.containsKey(m));
       //System.out.println(children);
       children.get(m).firstPlayer(c);
     }
